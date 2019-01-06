7ecbfef3277adff5117cee997ad885881a8d6314
Brandon <brandonjamesparkinson>
2019-01-06 14:15:36 +0000

    added ability to draw a polygon

--- GraphicalProgram/Form1.cs
+++ GraphicalProgram/Form1.cs
@@ -127,7 +127,29 @@ namespace GraphicalProgram
             //functionality for drawing Polygon to canvas 
             if (input.Contains("Polygon"))
             {
-                
+
+                // Create points that define polygon.
+                Point point1 = new Point(50, 50);
+                Point point2 = new Point(100, 25);
+                Point point3 = new Point(200, 5);
+                Point point4 = new Point(250, 50);
+                Point point5 = new Point(300, 100);
+                Point point6 = new Point(350, 200);
+                Point point7 = new Point(250, 250);
+                Point[] curvePoints =
+                {
+                    point1,
+                    point2,
+                    point3,
+                    point4,
+                    point5,
+                    point6,
+                    point7
+                };
+
+                // Draw polygon to screen.
+                g.DrawPolygon(pen, curvePoints);
+
             }
 
             //functionality for pen up command 
